53e2321ce7189d8dcb8e28b42e73f20d0b0415ad
Fcerron <86371231+Fcerron@users.noreply.github.com>
2022-07-23 22:39:36 -0300

    Add files via upload

---
+++ desafiodio-poo/src/br/com/dio/desafio/dominio/hellogit.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package br.com.dio.desafio.dominio;
+
+/**
+ * @author seiya
+ *
+ */
+public class hellogit {
+
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
